83e3ff1ce822988d29041f99eb909fe3ff162ca3
Vitalii Kyian <vitalii.kyian@masterofcode.com>
2020-07-21 17:48:47 +0300

    styling the blog

--- src/app/modules/blog/post-comments/post-comments.component.ts
+++ src/app/modules/blog/post-comments/post-comments.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Comment } from "../../../interfaces/comment.interface";
 import { ActivatedRoute } from "@angular/router";
 import { BlogService } from "../../../services/blog.service";
+import {Post} from "../../../interfaces/post.interface";
 
 @Component({
   selector: 'app-post-comments',
@@ -11,6 +12,7 @@ import { BlogService } from "../../../services/blog.service";
 export class PostCommentsComponent implements OnInit {
 
   comments: Comment[] = [];
+  post: Post;
 
   constructor( private blogService: BlogService,
                private route: ActivatedRoute) { }
@@ -18,6 +20,14 @@ export class PostCommentsComponent implements OnInit {
   ngOnInit(): void {
 
     const postId: string = this.route.snapshot.paramMap.get('id');
+
+    this.blogService.getPost(postId)
+      .subscribe(
+        (post: Post) => {
+        this.post= post;
+      }
+    );
+
     this.blogService.getComments(postId)
       .subscribe(
         (comments: Comment[]) => {
